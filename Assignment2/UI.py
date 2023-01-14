2cb12121a63ef13fe259069636fcfc480925ab4b
Raymond Chen <haojunchen51@gmail.com>
2023-01-13 19:00:22 -0800

    1/13/2022

--- Assignment2/UI.py
+++ Assignment2/UI.py
@@ -3,37 +3,39 @@ import os
 import sys
 import time
 #running process
+
+
+
 while(True):
     userInput = input("Enter 1 to get a random image or 2 to exist the program: ")
     if(int(userInput) == 1):
         
         #getting number from the PRNG.py
-        file = open(os.path.join(sys.path[0], "PRNG_Service.txt"), "r+") 
-        file.seek(0)
-        file.write("run")
-        file.seek(0)
+        fileN = open(os.path.join(sys.path[0], "PRNG_Service.txt"), "w+") 
+        fileN.seek(0)
+        fileN.write("run")
+        fileN.seek(0)
         time.sleep(0.5)
-        imageNumber = file.read()
-        #print(imageNumber)
-        file.close()
+        imageNumber = fileN.read()
+        print(imageNumber)
+        fileN.close()
 
-        #getting images from the image.py
-        file = open(os.path.join(sys.path[0], "image_service.txt"), "w+") 
-        file.seek(0)
-        #calling the image.py
-        file.write(str(imageNumber))
-        time.sleep(1.5)
-        #get the path
-        path = str(file.read())
-        print(path)
-        file.close()
+        
+        #send number to image.py
+        fileI = open(os.path.join(sys.path[0], "image_service.txt"), "w+") 
+        fileI.write(str(imageNumber))
+
+        fileI.close()
     else:
         #end image.py 
-        file = open(os.path.join(sys.path[0], "image_service.txt"), "w+") 
-        file.seek(0)
-        file.write("end")
+        #file.seek(0)
+        fileI = open(os.path.join(sys.path[0], "image_service.txt"), "w+") 
+        fileI.write("end")
+        fileI.close()
         #end PRNG.py
-        file = open(os.path.join(sys.path[0], "PRNG_Service.txt"), "r+") 
-        file.seek(0)
-        file.write("end")
+        fileN = open(os.path.join(sys.path[0], "PRNG_Service.txt"), "w+") 
+        fileN.seek(0)
+        fileN.write("end")
+        fileN.close()
         break
+
